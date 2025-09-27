074a0b8fd0f6d9220cb0f94c5746e5858c5b1a44
Klematisy <101270054+Klematisy@users.noreply.github.com>
2025-09-27 16:33:14 +0300

    clearing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include <Draw/draw.h>
 #include <Geometry/mesh.h>
 
@@ -7,8 +8,11 @@ static GlCore::Window window(640, 640, "glib");
 int main() {
     GLIB_NAMESPACE_USING;
 
+    using namespace std::chrono_literals;
+
     Draw draw(window);
     Texture tex("resources/images/wonam.jpg");
+    Shader shader("resources/shaders/user.glsl");
 
     Geom::Mesh quad = Geom::MeshFactory::Get().CreateMesh("quad");
 
@@ -21,25 +25,38 @@ int main() {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    auto start = std::chrono::high_resolution_clock::now();
+
+    float *point = &rotation.x;
+
     while (window.IsOpen()) {
         draw.Start();
 
-        if (glfwGetKey(window.GetWindow(), GLFW_KEY_J) == GLFW_PRESS) rotation.x -= 0.6f;
-        if (glfwGetKey(window.GetWindow(), GLFW_KEY_L) == GLFW_PRESS) rotation.x += 0.6f;
-        if (glfwGetKey(window.GetWindow(), GLFW_KEY_I) == GLFW_PRESS) rotation.y -= 0.6f;
-        if (glfwGetKey(window.GetWindow(), GLFW_KEY_K) == GLFW_PRESS) rotation.y += 0.6f;
-        if (glfwGetKey(window.GetWindow(), GLFW_KEY_U) == GLFW_PRESS) rotation.z -= 0.6f;
-        if (glfwGetKey(window.GetWindow(), GLFW_KEY_O) == GLFW_PRESS) rotation.z += 0.6f;
+        if (glfwGetKey(window.GetWindow(), GLFW_KEY_X) == GLFW_PRESS) point = &rotation.x;
+        if (glfwGetKey(window.GetWindow(), GLFW_KEY_Y) == GLFW_PRESS) point = &rotation.y;
+        if (glfwGetKey(window.GetWindow(), GLFW_KEY_Z) == GLFW_PRESS) point = &rotation.z;
+
+        if (glfwGetKey(window.GetWindow(), GLFW_KEY_LEFT) == GLFW_PRESS)  *point -= 0.6f;
+        if (glfwGetKey(window.GetWindow(), GLFW_KEY_RIGHT) == GLFW_PRESS) *point += 0.6f;
 
         if (glfwGetKey(window.GetWindow(), GLFW_KEY_W) == GLFW_PRESS) pos.y -= speed;
         if (glfwGetKey(window.GetWindow(), GLFW_KEY_S) == GLFW_PRESS) pos.y += speed;
         if (glfwGetKey(window.GetWindow(), GLFW_KEY_A) == GLFW_PRESS) pos.x -= speed;
         if (glfwGetKey(window.GetWindow(), GLFW_KEY_D) == GLFW_PRESS) pos.x += speed;
 
+        if (glfwGetKey(window.GetWindow(), GLFW_KEY_R) == GLFW_PRESS) {
+            GlCore::ShaderCache::GetCache().HotReload();
+            std::this_thread::sleep_for(200ms);
+        }
 
         quad.SetPosition(pos);
-        draw.DrawMesh(quad, {1.0f, 1.0f, 1.0f});
         quad.SetRotation(rotation);
+
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> dur = end - start;
+        shader.GetShader().SetUniform1f("u_Time", dur.count());
+
+        draw.DrawMesh(quad, {1.0f, 1.0f, 1.0f}, &tex, &shader);
 
         draw.End();
     }
