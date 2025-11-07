d3626356ded5bb5c5e87f11971c0132f68d04f42
Klematisy <101270054+Klematisy@users.noreply.github.com>
2025-11-07 23:45:40 +0300

    tst

--- windows/build_dependencies.bat
+++ windows/build_dependencies.bat
@@ -1,15 +1,22 @@
 set BUILD_TYPE=%~1
 
-set ZLIB_LIB=-DZLIB_LIBRARY=%cd%\extdeps\zlib\lib\zlibd.lib -DZLIB_INCLUDE_DIR=%cd%\extdeps\zlib\include
-set PNG_LIB=-DPNG_LIBRARY=%cd%\extdeps\libpng\lib\libpng16d.lib -DPNG_PNG_INCLUDE_DIR=%cd%\extdeps\libpng\include
-set BZIP2_LIB=-DBZIP2_LIBRARIES=%cd%\extdeps\BZip2\lib\bz2.lib -DBZIP2_INCLUDE_DIR=%cd%\extdeps\BZip2\include
+set BUILD_SYMBOL=""
+
+if "%BUILD_TYPE%"=="Debug" (
+    set BUILD_SYMBOL="d"
+)
+
+set ZLIB_LIB=-DZLIB_LIBRARY=%cd%\extdeps\zlib\lib\zlib%BUILD_SYMBOL%.lib -DZLIB_INCLUDE_DIR=%cd%\extdeps\zlib\include
+set PNG_LIB=-DPNG_LIBRARY=%cd%\extdeps\libpng\lib\libpng16%BUILD_SYMBOL%.lib -DPNG_PNG_INCLUDE_DIR=%cd%\extdeps\libpng\include
+set BZIP2_LIB=-DBZIP2_LIBRARIES=%cd%\extdeps\BZip2\lib\bz2.dll -DBZIP2_INCLUDE_DIR=%cd%\extdeps\BZip2\include
 set BROTLI_LIB=-DBROTLIDEC_LIBRARIES=%cd%\extdeps\brotli\lib\brotlidec.lib -DBROTLIDEC_INCLUDE_DIRS=%cd%\extdeps\brotli\include
 set HarfBuzz_LIB=-DHarfBuzz_LIBRARIES=%cd%\extdeps\HarfBuzz\lib\harfbuzz.lib -DHarfBuzz_INCLUDE_DIR=%cd%\extdeps\HarfBuzz\include
 
-set BUILD_SYMBOL=""
+set BZIP2_LIB_TYPE=-DBZIP2_LIBRARY_RELEASE=%cd%extdeps/BZip2/lib/bz2.lib
 
-if %BUILD_TYPE%=="Debug" (
-    set BUILD_SYMBOL="d"
+if "%BUILD_TYPE%"=="Debug" (
+	set BZIP2_LIB_TYPE=-DBZIP2_LIBRARY_DEBUG=%cd%extdeps/BZip2/lib/bz2.lib
+	set BUILD_SYMBOL=d
 )
 
 del extdeps
@@ -17,35 +24,39 @@ mkdir extdeps
 
 cmake -S downloads\brotli -B downloads\brotli\build ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
-    -DCMAKE_INSTALL_PREFIX=extdeps\brotli
+    -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\brotli"
 
 cmake --build downloads\brotli\build --parallel
 cmake --install downloads\brotli\build --config %BUILD_TYPE%
 
 cmake -S downloads\BZip2 -B downloads\BZip2\build ^
-    -DCMAKE_BUILD_TYPE=$(BUILD_TYPE) ^
-    -DCMAKE_INSTALL_PREFIX=extdeps\BZip2
+    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+    %BZIP2_LIB_TYPE% ^
+    -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\BZip2"
 
 cmake --build downloads\BZip2\build --parallel
-cmake --install downloads\BZip2\build --prefix extdeps\BZip2 --config $(BUILD_TYPE)
+cmake --install downloads\BZip2\build --config %BUILD_TYPE%
 
 cmake -S downloads\zlib -B downloads\zlib\build ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
-    -DCMAKE_INSTALL_PREFIX="extdeps\zlib"
+    -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\zlib"
 
 cmake --build downloads\zlib\build --parallel
 cmake --install downloads\zlib\build --config %BUILD_TYPE%
 
 cmake -S downloads\libpng -B downloads\libpng\build ^
-    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% %ZLIB_LIB%
+    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% %ZLIB_LIB% ^
+    -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\libpng"
 
 cmake --build downloads\libpng\build --parallel
-cmake --install downloads\libpng\build --prefix extdeps\libpng --config %BUILD_TYPE%
+cmake --install downloads\libpng\build --config %BUILD_TYPE%
 
 cmake -S downloads\HarfBuzz -B downloads\HarfBuzz\build ^
-    -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
+    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+    -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\HarfBuzz"
+
 cmake --build downloads\HarfBuzz\build --parallel
-cmake --install downloads\HarfBuzz\build --prefix extdeps\HarfBuzz --config %BUILD_TYPE%
+cmake --install downloads\HarfBuzz\build --config %BUILD_TYPE%
 
 cmake -S downloads\freetype -B downloads\freetype\build ^
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ^
@@ -53,8 +64,8 @@ cmake -S downloads\freetype -B downloads\freetype\build ^
      %PNG_LIB% ^
      %BROTLI_LIB% ^
      %BZIP2_LIB% ^
-    -DCMAKE_INSTALL_PREFIX=extdeps\freetype ^
-    -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
+    -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\freetype" ^
+    -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DBUILD_SHARED_LIBS=ON
 
 cmake --build downloads\freetype\build --parallel
 cmake --install downloads\freetype\build --config %BUILD_TYPE%
