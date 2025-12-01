230fcb732c273f35728d952b32c74d6695aa80c3
Klematisy <101270054+Klematisy@users.noreply.github.com>
2025-12-01 23:00:46 +0300

    windows

--- windows/build_dependencies.bat
+++ windows/build_dependencies.bat
@@ -20,7 +20,7 @@ cmake -S downloads\brotli -B downloads\brotli\build ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\brotli" ^
     -DBROTLI_BUILD_FOR_PACKAGE=ON
-cmake --build downloads\brotli\build --parallel
+cmake --build downloads\brotli\build --parallel --config %BUILD_TYPE%
 cmake --install downloads\brotli\build --config %BUILD_TYPE%
 
 
@@ -34,14 +34,14 @@ cmake -S downloads\BZip2 -B downloads\BZip2\build ^
     -DENABLE_TESTS=OFF ^
     -DENABLE_EXAMPLES=OFF ^
     -DENABLE_DOCS=OFF
-cmake --build downloads\BZip2\build --parallel
+cmake --build downloads\BZip2\build --parallel --config %BUILD_TYPE%
 cmake --install downloads\BZip2\build --config %BUILD_TYPE%
 
 
 cmake -S downloads\zlib -B downloads\zlib\build ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\zlib"
-cmake --build downloads\zlib\build --parallel
+cmake --build downloads\zlib\build --parallel --config %BUILD_TYPE%
 cmake --install downloads\zlib\build --config %BUILD_TYPE%
 
 
@@ -49,7 +49,7 @@ cmake -S downloads\libpng -B downloads\libpng\build ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% %ZLIB_LIB% ^
     -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\libpng" ^
     -DPNG_SHARED=OFF -DPNG_STATIC=ON
-cmake --build downloads\libpng\build --parallel
+cmake --build downloads\libpng\build --parallel --config %BUILD_TYPE%
 cmake --install downloads\libpng\build --config %BUILD_TYPE%
 
 
@@ -57,7 +57,7 @@ cmake -S downloads\HarfBuzz -B downloads\HarfBuzz\build ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\HarfBuzz" ^
     -DBUILD_SHARED_LIBS=OFF
-cmake --build downloads\HarfBuzz\build --parallel
+cmake --build downloads\HarfBuzz\build --parallel --config %BUILD_TYPE%
 cmake --install downloads\HarfBuzz\build --config %BUILD_TYPE%
 
 
@@ -71,7 +71,7 @@ cmake -S downloads\freetype -B downloads\freetype\build ^
     -DCMAKE_INSTALL_PREFIX="%cd%\extdeps\freetype" ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DBUILD_SHARED_LIBS=OFF -DFT_REQUIRE_BZIP2=ON
-cmake --build downloads\freetype\build --parallel
+cmake --build downloads\freetype\build --parallel --config %BUILD_TYPE%
 cmake --install downloads\freetype\build --config %BUILD_TYPE%
 
 
@@ -85,7 +85,7 @@ cmake -S downloads\msdf-atlas-gen -B downloads\msdf-atlas-gen\build ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DMSDF_ATLAS_USE_VCPKG=OFF -DMSDF_ATLAS_USE_SKIA=OFF ^
     -DMSDF_ATLAS_DYNAMIC_RUNTIME=ON -DBUILD_SHARED_LIBS=OFF -DMSDF_ATLAS_BUILD_STANDALONE=OFF
-cmake --build downloads\msdf-atlas-gen\build --parallel
+cmake --build downloads\msdf-atlas-gen\build --parallel --config %BUILD_TYPE%
 
 
 xcopy downloads\msdf-atlas-gen extdeps\msdf-atlas-gen /E /I /H /Y
